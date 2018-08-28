5e4810a35e1948c0e12bc0b2e4685c6eb72e11d8
Alexander Schneider <alexander@schneider.gg>
2018-08-28 22:31:51 +0200

    Add functions to read edn and to draw a uniform weighted object

--- friendly-backend/src/friendly_backend/core.clj
+++ friendly-backend/src/friendly_backend/core.clj
@@ -1,5 +1,31 @@
 (ns friendly-backend.core
-  (:require [postal.core :as postal]))
+  (:require [postal.core :as postal]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+(defn load-edn
+  "Load edn from an io/reader source (filename or io/resource)."
+  [source]
+  (try
+    (with-open [r (io/reader source)]
+      (edn/read (java.io.PushbackReader. r)))
+
+    (catch java.io.IOException e
+      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
+    (catch RuntimeException e
+      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
+
+(defn weighted-uniform-draw
+  "Draws a random uniform oject by weight from the collection. `weight-key` denominates which key in the map is refering to the weight."
+  [contacts weight-key]
+  (let [weights (reductions + (map (keyword weight-key) contacts))
+        total (last weights)
+        choices (map vector contacts weights)]
+    (let [choice (rand-int total)]
+      (loop [[[contact weight] & more] choices]
+        (if (< choice weight)
+          contact
+          (recur more))))))
 
 (defn foo
   "I don't do a whole lot."
@@ -13,7 +39,6 @@
                         :subject "Test friends"
                         :body "Heyaa contact your friends."}))
 
-
 (defn -main
-  []
-  (foo "message"))
+  [& args]
+  (print "Hello friends"))
